f845097ed4cd1ac1d6040f437e823d8f1dcb4e15
FranciscoHu17 <francisco.hu17@gmail.com>
2021-04-23 14:05:51 -0400

    added skeleton functions for the class

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # CSE352_Final_Project
 
 ## How to run:
+If you're on MacOS, following the installation steps on Windows should work, but it has not been tested yet.
+
 If on Windows: 
 - Install Anaconda at https://www.anaconda.com/products/individual
 - Verify your installation in Anaconda Prompt with the command ```conda list``` which should display a list of installed packages
 - Also verify that python is working by entering the command ```python```
-- Install ```gym``` with the command ```conda install -c conda-forge gym```
+- Install ```gym``` by running ```conda install -c conda-forge gym```
+- Install ```tensorflow``` by runnning ```py -m pip install tensorflow```
 - Navigate to the location of the walker.py file and run ```py walker.py```
 
 If on Linux:
-- Run ```pip install gym``` in the terminal
-- Navigate to the location of the walker.py file and run ```py walker.py```
+- Install ```gym``` by running ```pip install gym``` in the terminal
+- Install ```tensorflow``` by runnning ```pip install tensorflow```
+- Navigate to the location of the walker.py file and run ```python walker.py```
 
 More information on installing OpenAI Gym API at https://github.com/openai/gym#installation
--- walker.py
+++ walker.py
@@ -1,14 +1,62 @@
 import gym
-env = gym.make('BipedalWalker-v3')
+import numpy as np
+import tensorflow as tf
+from statistics import mean, median
+from collections import Counter
 
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        #print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
-env.close()
+def simulate():
+    training = []
+    scores = []
+    accepted = []
+    for game in range(games):
+        score = 0
+        curr_run = []
+        observation = env.reset()
+        
+        for t in range(goal_steps):
+            #env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+
+            curr_run.append([observation, action])
+            score += reward
+            
+            if done:
+                # Agent fell to the ground
+                print("Episode finished after {} timesteps".format(t+1))
+                break
+        
+        scores.append(score)
+        if score >= score_req:
+            accepted.append(score)
+            for data in curr_run:
+                training.append(data)
+        
+    env.close()
+
+    
+    #neural_network(np.array(training), np.array(accepted))
+
+    #print(Counter(accepted))
+    print("Accepted Mean:", mean(accepted))
+    print("Accepted Median:", median(accepted))
+    print("All Mean:", mean(scores))
+    print("All Median:", median(scores))
+
+
+def neural_network(data, accepted):
+    dataset = tf.data.Dataset.from_tensor_slices((data, accepted))
+
+def train_model(data):
+    X = 1
+
+if '__main__' == __name__:
+    env = gym.make('BipedalWalker-v3')
+
+    LR = 1e-3   # Learning Rate
+    goal_steps = 500
+    score_req = -100
+    games = 10
+    
+    simulate()
+
