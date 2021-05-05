516d52b8baf5755cc4e1303aa7b850d0a8ca828e
Eddie X <eddiex2205@gmail.com>
2021-05-05 18:57:15 -0400

    baseline models for 200, 300, 400 eps

--- td3_walker.py
+++ td3_walker.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 import gym
 import torch
 import random
@@ -8,6 +9,15 @@ import matplotlib.pyplot as plt
 
 def main():
     env = gym.make('BipedalWalker-v3')
+
+    # set seed for reproducable results
+    seed = 1
+    env.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    random.seed(seed)
+
+
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
     max_action = float(env.action_space.high[0])
@@ -17,26 +27,24 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     policy = TD3(state_dim, action_dim, max_action, env, device)
-    try:
-        policy.load()
-    except Exception as e:
-        print('No previous model to load.')
+    # try:
+    #     policy.load()
+    # except Exception as e:
+    #     print('No previous model to load.')
 
     buffer = ExperienceReplay(buffer_size, batch_size, device)
 
-    episodes = 5000
+    episodes = 650
     timesteps = 2000
 
-    best_reward = 0
-    curr_reward = []
+    best_reward = -1*sys.maxsize
+    scores_over_episodes = []
 
     for episode in range(episodes):
         avg_reward = 0
         state = env.reset()
         for i in range(timesteps):
             # Same as the TD3, select an action and add noise:
-            if(i % 200 == 0):
-                print('Finished', i, 'timesteps')
             action = policy.select_action(state) + np.random.normal(0, max_action * noise, size=action_dim)
             action = action.clip(env.action_space.low, env.action_space.high)
             # Make an action. 
@@ -48,29 +56,26 @@ def main():
             if(len(buffer) > batch_size):
                 policy.train(buffer, i)
             if(done or i > timesteps):
+                scores_over_episodes.append(avg_reward)
                 print('Episode ', episode,'finished with reward:', avg_reward)
                 print('Finished at timestep ', i)
-                curr_reward.append(avg_reward)
                 break
-        print('Finished 2000 timesteps')
-        if(np.mean(curr_reward[-100:]) >= 300):
+        if(np.mean(scores_over_episodes[-50:]) > 350):
+            print('Saving agent- past 50 scores gave better average score than +350.')
+            best_reward = np.mean(scores_over_episodes[-50:])
             policy.save()
-            break
+            break # Saved agent so far. Break out of episodes and end. 
 
-        if((episode % 100 == 0 and episode > 0) or avg_reward > best_reward):
+        if(episode >= 0 and avg_reward > best_reward):
+            print('Saving agent- score for this episode was better than best-known score..')
             best_reward = avg_reward
-            #Save policy and optimizer every 100 episodes
-            policy.save()
+            policy.save() # Save current policy + optimizer
     
-
-    # fig = plt.figure()
-    # plt.plot(np.arange(1, len(ep_reward) + 1), ep_reward)
-    # plt.ylabel('Score')
-    # plt.xlabel('Episode #')
-    # plt.show()
-
-    
-
+    fig = plt.figure()
+    plt.plot(np.arange(1, len(scores_over_episodes) + 1), scores_over_episodes)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.show()
 
 
 main()
